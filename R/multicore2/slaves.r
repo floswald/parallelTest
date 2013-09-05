c63585b0cff19213c99b532f60c9e1244a52b653
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2013-09-05 10:26:16 +0100

    automatically fecth R library path

--- R/multicore2/slaves.r
+++ R/multicore2/slaves.r
@@ -1,6 +1,10 @@
 ## R implementation of recursive Fibonacci sequence
-fibR <- function(n) {
 
+N = 1000
+B = matrix(runif(N^2),N,N)
+
+fibR <- function(n) {
+    B <<- B + 1
     if (n == 0) return(0)
     if (n == 1) return(1)
     return (fibR(n - 1) + fibR(n - 2))

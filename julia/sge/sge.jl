82d543001ecdaf7816474e0673b5893e12a27c91
Florian Oswald <florian.oswald@gmail.com>
2014-06-30 10:02:28 +0100

    added workers() call

--- julia/sge/sge.jl
+++ julia/sge/sge.jl
@@ -45,6 +45,8 @@ println("Started julia")
 bind_pe_procs()
 
 println(" trying parallel for loop with $(nprocs()) processes")
+println("numworkers: $(length(workers()))")
+println("workers: $(workers())")
 @time map( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
 @time pmap( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
 

52aacfc79061d373782c6440123f19705663ca25
lionup@gmail.com <lionup@gmail.com>
2014-08-03 17:25:38 +0100

    legion

--- R/legion/exp.sh
+++ R/legion/exp.sh
@@ -1,16 +1,4 @@
-#!/bin/bash 
-
-echo "starting my qsub file"
-source ~/.bash_profile
-date
-
-# Batch script to run an OpenMP threaded R job on Legion with the upgraded
-# software stack under SGE. Using the forech packages foreach(...) %dopar%
-# for example. 
-
-# This version works with the modules environment upgraded in Feb 2012.
-
-# R Version 2.15.2
+#!/bin/bash -l
 
 # 1. Force bash as the executing shell.
 #$ -S /bin/bash 
@@ -22,12 +10,15 @@ date
 # 3. Request 1 gigabyte of RAM. Change this to suit your requirements.
 #$ -l mem=1G
 
-# 4. Set the name of the job. You can change this if you wish.
+# 4. Request 15 gigabyte of TMPDIR space per node (default is 10 GB)
+#$ -l tmpfs=15G
+
+# 5. Set the name of the job. You can change this if you wish.
 #$ -N R_jobMC_2
 
-# 5. Select the project that this job will run under.
+# 6. Select the project that this job will run under.
 # Find <your_project_id> by running the command "groups"
-#$ -P bankruptcy
+#$ -P laborhome
 
 # 7. Set the working directory to somewhere in your scratch space.  This is
 # a necessary step with the upgraded software stack as compute nodes cannot
@@ -36,26 +27,14 @@ date
 # NOTE: this directory must exist.
 #
 # Replace "<your_UCL_id>" with your UCL user ID :)
-#$ -wd /home/uctpfos/Scratch/git/mpitest/R/legion
+#$ -wd $HOME/Scratch/git/mpitest/R/legion
 
-# 6. select messaging option
-#$ -M f.oswald@ucl.ac.uk	# send notifications to
-#$ -m e # notify about end of job
+# 8. Select parallel environment 
+#$ -pe qlc 8
 
-# 7. Select the OpenMPI parallel environment and 16 processes.
-#$ -pe openmpi 10
-
-# 8. set error out files
-#$ -o exp.out # <- name of the output file.
-#$ -e exp.err # <- name of the stderr file.
-
-# 10. load modules
-module unload compilers/intel/11.1/072
-module unload mpi/qlogic/1.2.7/intel
-module unload mkl/10.2.5/035
+# 9. load modules
+module unload compilers
 module load recommended/r
-module load mpi/openmpi/1.4.5/gnu.4.6.3
-
 
 # 11. Run our MPI job.  GERun is a wrapper that launches MPI jobs on Legion
-gerun /home/uctpfos/R/x86_64-unknown-linux-gnu-library/2.15/snow/RMPISNOW -q < exp.r > exp.Rout
+gerun $HOME/R/x86_64-unknown-linux-gnu-library/3.0/snow/RMPISNOW -q < $HOME/git/mpitest/R/legion/exp.r > r.output.$JOB_ID

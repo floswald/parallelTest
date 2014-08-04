5a5585cdf0f540556f8264f73a35946795a8dffb
Florian Oswald <florian.oswald@gmail.com>
2014-08-04 16:45:57 +0000

    no way to get environment vars on compute nodes. annyoing.

--- julia/sherlock/submit.sh
+++ julia/sherlock/submit.sh
@@ -1,15 +1,8 @@
-#!/bin/bash
+#!/bin/bash -l
 
-echo "starting qsub script file"
-date
 
-# my definitions
-source ~/.bash_profile
-
-# here's the SGE directives
-# ------------------------------------------
-#$ -pe impi 8  #  <- load the impi parallel env 
 #$ -S /bin/bash   # <- run the job under bash
+#$ -pe impi 8  #  <- load the impi parallel env 
 #$ -N mpi-testing # <- name of the job in the qstat output
 #$ -o timer.out # direct output stream to here
 #$ -e timer.err # <- name of the stderr file.
@@ -19,7 +12,11 @@ echo "here are your hosts"
 
 cat $PE_HOSTFILE
 
+source /home/florian_o/settings.sh
 
+echo "my library path is"
+echo $LD_LIBRARY_PATH
 echo "calling julia now"
 
-julia /home/florian_o/git/mpitest/julia/sherlock/sge.jl
+
+/home/florian_o/local/bin/julia /home/florian_o/git/mpitest/julia/sherlock/sge.jl

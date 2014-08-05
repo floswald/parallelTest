61474ec7837529a5a6fa27478c04481165ddbb99
Florian Oswald <florian.oswald@gmail.com>
2014-08-05 16:03:46 +0100

    can't see libraries?

--- julia/iridis/submit.sh
+++ julia/iridis/submit.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 #PBS -S /bin/bash
 #PBS -q consort
-#PBS -l nodes=4:ppn=12
-#PBS -l walltime=06:00
+#PBS -l nodes=2:ppn=12
+#PBS -l walltime=04:00
 #PBS -o mpi_test.out
 #PBS -e mpi_test.err
 
@@ -13,12 +13,18 @@ cd $PBS_O_WORKDIR
 # load openmpi module so that we find the mpirun command
 # module load openmpi
 
+echo "loaded modules"
+module list
+
 # load all modules required for julia
+module unload gcc
 module load gcc/4.8.1
-module load python
 
 echo "here is your PBS_NODEFILE"
 cat $PBS_NODEFILE
+
+echo "loaded modules"
+module list
 
 # Run matmul executable in parallel over number of nodes/processors requested
 # by job (2*8 by default in #PBS line above), output messages go to output_file

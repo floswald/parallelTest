95fd68c74063da78c180336185942d47f33543d3
Florian Oswald <florian.oswald@gmail.com>
2014-08-13 12:15:26 +0100

    cleaned up submit script

--- julia/iridis/submit.sh
+++ julia/iridis/submit.sh
@@ -1,4 +1,3 @@
-	
 #!/bin/bash
 #PBS -S /bin/bash
 #PBS -q consort
@@ -12,27 +11,17 @@ date
 # Change to directory from which job was submitted
 cd $PBS_O_WORKDIR
 
+# loaded all modules required for julia
+# in .bash_profile
+
 echo "loaded modules"
 module list
-
-# load all modules required for julia
-# in .bash_profile
-# module unload gcc
-# module initadd gcc/4.8.1
 
 echo "here is your PBS_NODEFILE"
 cat $PBS_NODEFILE
 
-echo "library path"
+echo "check library path has gcc/4.8.1/lib64 libraries"
 echo $LD_LIBRARY_PATH
 
-# Run matmul executable in parallel over number of nodes/processors requested
-# by job (2*8 by default in #PBS line above), output messages go to output_file
-
-# mpirun matmul > output_file
-
-# LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/eisuc151/git/julia/usr/lib
-
 echo "calling julia now"
-# LD_LIBRARY_PATH=$LD_LIBRARY_PATH /home/eisuc151/local/bin/julia sge.jl 
 /home/eisuc151/local/bin/julia sge.jl 

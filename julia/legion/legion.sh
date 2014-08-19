00f1a2003346d426bc280ca44129797f8a4f6383
Florian Oswald <florian.oswald@gmail.com>
2014-08-19 21:53:02 +0100

    legion script

--- julia/legion/legion.sh
+++ julia/legion/legion.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -l
-
-
 #$ -S /bin/bash
 #$ -l h_rt=0:05:0
 #$ -l mem=1G
@@ -16,10 +14,6 @@ date
 
 echo $PE_HOSTFILE
 
-module unload compilers
-module load compilers/gnu/4.6.3
-module load python/2.7.3
-
 echo "launching julia"
 
-/home/uctpfos/git/julia/julia /home/uctpfos/git/mpitest/julia/legion/sge.jl
+/home/uctpfos/git/julia/julia /home/uctpfos/git/parallelTest/julia/legion/sge.jl

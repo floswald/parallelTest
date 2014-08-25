ae540d364e1ebf217472a9fe9df0b073c9258f2f
Florian Oswald <florian.oswald@gmail.com>
2014-08-25 19:23:48 +0100

    add version that reserves the full node but only starts ppn processes

--- julia/iridis/iridis_launcher.jl
+++ julia/iridis/iridis_launcher.jl
@@ -44,9 +44,62 @@ function bind_iridis_procs()
 
     # add to julia home
     println("adding machines to JULIA_HOME: $JULIA_HOME")
-
     for m in machines
         addprocs([m], dir= JULIA_HOME)
     end
+
     println("done")
 end
+
+function bind_iridis_procs(ppn::Int)
+
+    # add only ppn processes per node
+
+    home = ENV["HOME"]
+
+     # find out what cluster we are on
+    node_file_name = ENV["PBS_NODEFILE"]
+
+    # read PBS_NODEFILE
+    filestream = open(node_file_name)
+    seekstart(filestream)
+    node_file = readlines(filestream)
+
+    # strip eol
+    node_file = map(x->strip(x,['\n']),node_file)
+
+    # get number of workers on each node
+    procs = Dict{ASCIIString,Int}()
+    for n in node_file
+        procs[n] = get(procs,n,0) + 1
+    end
+
+    println("name of compute nodes and number of workers:")
+    println(procs)
+
+    # add processes on master itself
+    master = ENV["HOSTNAME"]
+
+    wrker = 0
+    while wrker < ppn
+        addprocs(1)
+        wrker += 1
+    end
+    println("added $wrker processes on master itself")
+
+    # add procs on other machines
+    for (k,v) in procs
+        wrker = 0
+        if k!=master
+            while wrker < ppn
+                addprocs(k, dir= JULIA_HOME)
+                # println("addprocs($k)")
+                wrker += 1
+            end
+            println("added $wrker processes on machine $k")
+        end
+    end
+
+    println("done")
+end
+
---
+++ julia/iridis/sge_8procs.jl
@@ -0,0 +1,34 @@
+
+println("Started julia on iridis")
+
+include("iridis_launcher.jl")
+
+bind_iridis_procs(8)
+
+# here a function that runs your estimation:
+# using MOpt, mig
+
+# require some code on all nodes
+require("../incl.jl")
+
+println("make everybody say hello")
+
+@everywhere sayhello()
+
+println("make everybody do some math")
+
+pmap( i->domath(i), [100 for j in 1:length(workers())] )
+
+println("make everybody pass a memory test")
+
+pmap( i->doBIGmath(), 1:length(workers()) )
+
+println("trying parallel for loop with $(nprocs()) processes")
+println("numworkers: $(length(workers()))")
+println("workers: $(workers())")
+@time map( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
+@time pmap( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:24]);
+
+println(" quitting ")
+
+quit()

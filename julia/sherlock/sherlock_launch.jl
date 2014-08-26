c2c8927043455f809b9313e9124a15ac2681a043
Florian Oswald <florian.oswald@gmail.com>
2014-08-26 14:43:02 +0100

    edit launcher

--- julia/sherlock/sherlock_launch.jl
+++ julia/sherlock/sherlock_launch.jl
@@ -10,13 +10,11 @@ function bind_sherlock_procs()
     seekstart(filestream)
     node_file = readlines(filestream)
 
-    # strip eol
-    node_file = map(x->strip(x,['\n']),node_file)
-
     # get number of workers on each node
     procs = Dict{ASCIIString,Int}()
-    for n in node_file
-        procs[n] = get(procs,n,0) + 1
+    for line in node_file
+        line_parts = split(line," ")
+        proc[line_parts[1]] = line_parts[2]
     end
 
     println("name of compute nodes and number of workers:")
@@ -30,22 +28,25 @@ function bind_sherlock_procs()
         println("added $(procs[master]-1) processes on master itself")
     end
 
-    # get a machine file for other hosts
-    machines = ASCIIString[]
-    for i in 1:length(node_file)
-        if node_file[i] != master
-            push!(machines,node_file[i])
+    # # get a machine file for other hosts
+    # machines = ASCIIString[]
+    # for i in 1:length(node_file)
+    #     if node_file[i] != master
+    #         push!(machines,node_file[i])
+    #     end
+    # end
+
+    # add procs on other machines
+    for (k,v) in procs
+        wrker = 0
+        if k!=master
+            while wrker < v
+                addprocs([k], dir= JULIA_HOME)
+                # println("addprocs($k)")
+                wrker += 1
+            end
+            println("added $wrker processes on machine $k")
         end
-    end
-
-
-    println("processes on worker machines:")
-    println(machines)
-
-    # add to julia home
-    println("adding machines to JULIA_HOME: $JULIA_HOME")
-    for m in machines
-        addprocs([m], dir= JULIA_HOME)
     end
 
     println("done")

a752715ac5a529d4dfe50bc6a6bbab0aa16aa256
Florian Oswald <florian.oswald@gmail.com>
2014-08-26 14:27:16 +0100

    adkist paths

--- julia/sherlock/submit.sh
+++ julia/sherlock/submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 #$ -S /bin/bash   # <- run the job under bash
-#$ -V   # forward environment vars
+#$ -V  
 #$ -pe impi_tight 16  
 #$ -q main.q
 #$ -N mpi-testing # <- name of the job in the qstat output
 #$ -o test.out # direct output stream to here
 #$ -e test.err # <- name of the stderr file.
-#$ -wd /home/florian_o/git/mpitest/julia/sherlock
+#$ -wd /home/florian_o/git/parallelTest/julia/sherlock
 
 echo "here are your hosts"
 
@@ -17,5 +17,5 @@ echo $LD_LIBRARY_PATH
 echo "calling julia now"
 
 
-/home/florian_o/local/bin/julia /home/florian_o/git/mpitest/julia/sherlock/sge.jl
+/home/florian_o/local/bin/julia /home/florian_o/git/paralleTest/julia/sherlock/sge.jl
 

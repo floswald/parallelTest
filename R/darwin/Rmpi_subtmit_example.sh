06cb3df8e408501ad5ad34ea6cff3aaeb5e89cc5
lionup <lionup@gmail.com>
2014-08-27 18:09:14 +0100

    d

--- R/darwin/Rmpi_subtmit_example.sh
+++ R/darwin/Rmpi_subtmit_example.sh
@@ -59,7 +59,7 @@ module add rmpi/0.6-5
 application="mpirun -np 1 Rscript"
 
 #! Run options for the application:
-options="helloWorld.R"
+options="exp.r"
 
 #! Work directory (i.e. where the job will run):
 workdir="$SLURM_SUBMIT_DIR"  # The value of SLURM_SUBMIT_DIR sets workdir to the directory
@@ -95,7 +95,7 @@ CMD="$application $options"
 #! Choose this for a MPI code (possibly using OpenMP) using OpenMPI:
 #CMD="mpirun -npernode $mpi_tasks_per_node -np $np $application $options"
 
-#CMD="mpirun -np $np /home/hpcgu1/R/x86_64-unknown-linux-gnu-library/3.1/snow/RMPISNOW -q < $options"
+CMD="mpirun -np 1 /home/hpcgu1/R/x86_64-unknown-linux-gnu-library/3.1/snow/RMPISNOW -q < $options"
 ###############################################################
 ### You should not have to change anything below this line ####
 ###############################################################

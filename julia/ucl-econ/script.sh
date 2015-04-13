d9ccf9ed453a8976bc3fe87eeb7fef6e1bebabe8
Florian Oswald <florian.oswald@gmail.com>
2015-04-13 16:17:39 +0100

    work on econ hpc

--- julia/ucl-econ/script.sh
+++ julia/ucl-econ/script.sh
@@ -5,7 +5,6 @@ source ~/.bash_profile
 date
 
 module load sge/2011.11
-module load julia/0.3.1
 
 # here's the SGE directives
 # ------------------------------------------
@@ -13,13 +12,15 @@ module load julia/0.3.1
 #$ -pe mpich 20  #  <- load the openmpi parallel env w/ $(arg1) slots
 #$ -S /bin/bash   # <- run the job under bash
 #$ -N mpi-testing # <- name of the job in the qstat output
-#$ -o timer.out # direct output stream to here
-#$ -e timer.err # <- name of the stderr file.
+#$ -o test.out # direct output stream to here
+#$ -e test.err # <- name of the stderr file.
 #$ -wd /data/uctpfos/git/parallelTest/julia/ucl-econ
 
 echo "calling mpirun now"
 
+cat "your DL_LIBRARY"
+echo $DL_LIBRARY_PATH
 
 cat $PE_HOSTFILE
 
-julia /data/uctpfos/git/parallelTest/julia/ucl-econ/sge.jl
+/data/uctpfos/git/julia/julia /data/uctpfos/git/parallelTest/julia/ucl-econ/sge.jl

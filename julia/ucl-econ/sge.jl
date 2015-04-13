5fa525dd1dfe4604342ebf9e8842a94c55a68dcf
Florian Oswald <florian.oswald@gmail.com>
2015-04-13 17:32:50 +0100

    undo comp

--- julia/ucl-econ/sge.jl
+++ julia/ucl-econ/sge.jl
@@ -10,11 +10,11 @@ ucl_econ_launcher()
 # using MOpt, mig
 # 
 
-println("trying parallel for loop with $(nprocs()) processes")
-println("numworkers: $(length(workers()))")
-println("workers: $(workers())")
-@time map( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
-@time pmap( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
+# println("trying parallel for loop with $(nprocs()) processes")
+# println("numworkers: $(length(workers()))")
+# println("workers: $(workers())")
+# @time map( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
+# @time pmap( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
 
 println(" quitting ")
 

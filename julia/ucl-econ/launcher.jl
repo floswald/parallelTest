e5435a9ded7f2d8a1220e9e80bd514ec3e7b5a82
Florian Oswald <florian.oswald@gmail.com>
2015-04-13 17:49:30 +0100

    addprocs

--- julia/ucl-econ/launcher.jl
+++ julia/ucl-econ/launcher.jl
@@ -47,8 +47,8 @@ function ucl_econ_launcher()
 
     # add to julia home
     println("adding machines to JULIA_HOME: $JULIA_HOME")
-    for m in machines
-        addprocs([m])
+    for m in 1:length(machines)
+        addprocs([machines[m]])
         sleep(1)
     end
 

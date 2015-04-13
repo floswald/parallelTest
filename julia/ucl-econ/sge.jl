580b1b99641b3132e7bd039e1ed2823bc59b520c
Florian Oswald <florian.oswald@gmail.com>
2015-04-13 16:41:23 +0100

    comment out

--- julia/ucl-econ/sge.jl
+++ julia/ucl-econ/sge.jl
@@ -46,17 +46,17 @@ end
 
 println("Started julia")
 
-bind_pe_procs()
+# bind_pe_procs()
 
 # here a function that runs your estimation:
 # using MOpt, mig
 # 
 
-println("trying parallel for loop with $(nprocs()) processes")
-println("numworkers: $(length(workers()))")
-println("workers: $(workers())")
-@time map( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
-@time pmap( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
+# println("trying parallel for loop with $(nprocs()) processes")
+# println("numworkers: $(length(workers()))")
+# println("workers: $(workers())")
+# @time map( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
+# @time pmap( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
 
 println(" quitting ")
 

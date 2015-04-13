2dcfdf535533e0d0d690743b6d7a6f132390c7a7
Florian Oswald <florian.oswald@gmail.com>
2015-04-13 17:25:06 +0100

    mistake

--- julia/ucl-econ/launcher.jl
+++ julia/ucl-econ/launcher.jl
@@ -48,7 +48,7 @@ function ucl_econ_launcher()
     # add to julia home
     println("adding machines to JULIA_HOME: $JULIA_HOME")
     # for m in machines
-        addprocs(m, dir= JULIA_HOME)
+        addprocs(machines, dir= JULIA_HOME)
     # end
 
     println("ucl_econ_launcher: done")

7a8923093fe3986ed564f790c98f6aba4236a757
lionup <lionup@gmail.com>
2015-05-08 18:55:29 +0100

    update R

--- R/clusterTime/timer.r
+++ R/clusterTime/timer.r
@@ -11,7 +11,7 @@ library(snow)
 reps <- 5	# how many repetitions
 
 # make cluster
-mycl <- makeMPIcluster()
+mycl <- getMPIcluster()
 
 num.worker <- length(clusterEvalQ(mycl,Sys.info()))	# num.worker chosen in submit script
 

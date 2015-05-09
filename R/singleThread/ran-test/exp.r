347dae04a3af054baa681fadda5b9d2fff64cd3b
lionup <lionup@gmail.com>
2015-05-09 16:50:06 +0100

    change R version

---
+++ R/singleThread/ran-test/exp.r
@@ -0,0 +1,6 @@
+
+install.packages("Rmpi",configure.args=c("--with-Rmpi-include=/cm/shared/apps/openmpi/gcc/64/1.4.5/include",
+                                         "--with-Rmpi-libpath=/cm/shared/apps/openmpi/gcc/64/1.4.5/lib64",
+                                         "--with-Rmpi-type=OPENMPI"))
+
+install.packages('snow')
---
+++ R/singleThread/ran-test/submit_R3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "starting qsub script file"
+source ~/.bash_profile
+date 
+
+# here's the SGE directives
+# ------------------------------------------
+#$ -q batch.q   # <- the name of the Q you want to submit to
+#$ -pe openmpi 3  #  <- load the openmpi parallel env w/ 6 slots
+#$ -S /bin/bash   # <- run the job under bash
+#$ -N exp-test # <- name of the job in the qstat output
+#$ -o exp.out # <- name of the output file.
+#$ -j y
+#$ -cwd
+
+module load gcc
+module load openmpi/gcc
+module load r/3.1.3
+
+echo "loaded modules"
+module list
+
+echo "calling mpirun now"
+mpirun -np 3 R --no-save -q < exp.r > exp.Rout
+
+

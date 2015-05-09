77147e3ea846560482ffe8761620e254ca841bb3
lionup <lionup@gmail.com>
2015-05-09 17:21:34 +0100

    change R version

--- R/singleThread/ran-test/exp.r
+++ R/singleThread/ran-test/exp.r
@@ -1,6 +1,5 @@
 
-install.packages("Rmpi",configure.args=c("--with-Rmpi-include=/cm/shared/apps/openmpi/gcc/64/1.4.5/include",
+install.packages("Rmpi",contrib.url(repos= "http://cran.us.r-project.org")),
+											 "configure.args=c("--with-Rmpi-include=/cm/shared/apps/openmpi/gcc/64/1.4.5/include",
                                          "--with-Rmpi-libpath=/cm/shared/apps/openmpi/gcc/64/1.4.5/lib64",
                                          "--with-Rmpi-type=OPENMPI"))
-
-install.packages('snow')

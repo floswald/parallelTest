061b3aa936b58e50a8d15356254cc506719a87de
lionup@gmail.com <lionup@gmail.com>
2015-05-12 14:58:38 +0100

    d

--- R/singleThread/ran-test/exp.r
+++ R/singleThread/ran-test/exp.r
@@ -1,5 +1,8 @@
+#
+#install.packages("Rmpi",configure.args=c("--with-Rmpi-include=/cm/shared/apps/openmpi/gcc/64/1.4.5/include",
+#                     "--with-Rmpi-libpath=/cm/shared/apps/openmpi/gcc/64/1.4.5/lib64",
+#                     "--with-Rmpi-type=OPENMPI"))
+#
 
-install.packages("Rmpi",configure.args=c("--with-Rmpi-include=/cm/shared/apps/openmpi/gcc/64/1.4.5/include",
-                     "--with-Rmpi-libpath=/cm/shared/apps/openmpi/gcc/64/1.4.5/lib64",
-                     "--with-Rmpi-type=OPENMPI"))
-require(Rmpi)
+
+install.packages("Rmpi",configure.args=c("--with-mpi=/cm/shared/apps/openmpi/gcc/64/1.4.5"))

7013f80fbf94812d2fb6e0d583ad1ead4c773e9c
lionup@gmail.com <lionup@gmail.com>
2015-05-12 15:25:38 +0100

    d

--- R/singleThread/ran-test/submit_R3.sh
+++ R/singleThread/ran-test/submit_R3.sh
@@ -7,7 +7,7 @@ date
 # here's the SGE directives
 # ------------------------------------------
 #$ -q batch.q   # <- the name of the Q you want to submit to
-#$ -pe openmpi 4  #  <- load the openmpi parallel env w/ 6 slots
+#$ -pe mpich 4  #  <- load the openmpi parallel env w/ 6 slots
 #$ -S /bin/bash   # <- run the job under bash
 #$ -N exp-test # <- name of the job in the qstat output
 #$ -o exp.out # <- name of the output file.
@@ -15,13 +15,14 @@ date
 #$ -cwd
 
 echo "loaded modules"
-module load gcc/0-test-4.9.0
-module load openmpi/gcc
+#module load gcc/0-test-4.9.0
+#module load openmpi/gcc
+module load mpich/ge/gcc
 module load r/3.1.3
 module list
 
 echo "MPI_ROOT"
-export MPI_ROOT=/cm/shared/apps/openmpi/gcc/64/1.4.5
+#export MPI_ROOT=/cm/shared/apps/openmpi/gcc/64/1.4.5
 printenv MPI_ROOT
 
 echo "LD_LIBRARY_PATH"

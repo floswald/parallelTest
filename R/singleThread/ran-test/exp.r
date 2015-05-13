42406f6a9d8c74bde50c8ae0df6a71ca236fc6c1
lionup@gmail.com <lionup@gmail.com>
2015-05-13 21:29:50 +0100

    d

--- R/singleThread/ran-test/exp.r
+++ R/singleThread/ran-test/exp.r
@@ -1,12 +1,14 @@
-library(snow)
+library(parallel)
 
 # start up cluster
-cl <- makeMPIcluster(7)
+cl <- makeCluster(7)
 # if you want to evaluate a funciton f, it needs to be defined on each slave
 f.long<-function(n) {
          xx<-rnorm(n)            
          log(abs(xx))+xx^2
 }
+system.time(sapply(rep(5E6,11),f.long))
+system.time(parSapply(cl,rep(5E6,11),f.long))
 system.time(sapply(rep(5E6,11),f.long))
 system.time(parSapply(cl,rep(5E6,11),f.long))
 

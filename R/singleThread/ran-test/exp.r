c81eca6a9b676b228bc50894168ff39e22535baf
lionup@gmail.com <lionup@gmail.com>
2015-05-13 15:54:24 +0100

    d

--- R/singleThread/ran-test/exp.r
+++ R/singleThread/ran-test/exp.r
@@ -1,19 +1,4 @@
 
-
-
-# R script. export data from master, execute a function collect results
-# load master libraries
-
-mpi.remote.exec(paste(Sys.info()[c("nodename")],"checking in as",mpi.comm.rank(),"of",mpi.comm.size()))
-
-require(snow)
-
-# require(RcppSimpleTensor)   
-
-print(date())
-# setup some data on master
-
-
 # start up cluster
 mycl <- makeCluster()
 print(clusterCall(mycl, function() Sys.info()))

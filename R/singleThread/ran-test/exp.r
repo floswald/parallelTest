88bc2b3cb33763f1791c4f06efd22a1a6c1a132d
lionup@gmail.com <lionup@gmail.com>
2015-05-13 21:54:43 +0100

    d

--- R/singleThread/ran-test/exp.r
+++ R/singleThread/ran-test/exp.r
@@ -1,7 +1,7 @@
 library(parallel)
 
 # start up cluster
-ncore <- 15
+ncore <- 31
 cl <- makeCluster(ncore)
 # if you want to evaluate a funciton f, it needs to be defined on each slave
 f.long<-function(n) {
@@ -9,8 +9,8 @@ f.long<-function(n) {
          log(abs(xx))+xx^2
 }
 
-system.time(sapply(rep(1E7,ncore),f.long))
-system.time(parSapply(cl,rep(1E7,ncore),f.long))
+system.time(sapply(rep(5E6,ncore),f.long))
+system.time(parSapply(cl,rep(5E6,ncore),f.long))
 
 stopCluster(cl)
 

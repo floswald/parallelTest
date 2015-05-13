c75e4bab0e1e3848a015960285abc9658b9ff7ac
lionup@gmail.com <lionup@gmail.com>
2015-05-13 14:18:50 +0100

    d

--- R/singleThread/ran-test/exp.r
+++ R/singleThread/ran-test/exp.r
@@ -8,4 +8,10 @@
 #install.packages("Rmpi",configure.args=c("--with-mpi=/cm/shared/apps/openmpi/gcc/64/1.4.5"))
 
 require(Rmpi)
+mpi.spawn.Rslaves(nslaves=2)
+# Tell all slaves to return a message identifying themselves
+mpi.remote.exec(paste(Sys.info()[c("nodename")],"checking in as",mpi.comm.rank(),"of",mpi.comm.size()))
 
+# Tell all slaves to close down, and exit the program
+mpi.close.Rslaves()
+mpi.quit()

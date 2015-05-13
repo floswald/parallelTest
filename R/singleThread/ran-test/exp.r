24029c2d84a07da16e680539309469db25b763a1
lionup@gmail.com <lionup@gmail.com>
2015-05-14 00:48:58 +0100

    d

--- R/singleThread/ran-test/exp.r
+++ R/singleThread/ran-test/exp.r
@@ -1,12 +1,12 @@
-library(Rmpi)
+#library(Rmpi)
 #library(parallel)
 library(snow)
 
 # start up cluster
-ncore <- mpi.universe.size()-1
-cat("ncore:",ncore,"\n")
-cl <- makeCluster(ncore)
-#cl <- getMPIcluster()
+#ncore <- mpi.universe.size()-1
+#cat("ncore:",ncore,"\n")
+cl <- makeCluster(type="MPI")
+ncore <- length(cl)
 
 # if you want to evaluate a funciton f, it needs to be defined on each slave
 f.long<-function(n) {

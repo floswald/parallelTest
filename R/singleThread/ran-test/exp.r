222d0c91ea458f7afcbdfdea0ef607cb8e05bc2f
lionup@gmail.com <lionup@gmail.com>
2015-05-13 21:38:03 +0100

    d

--- R/singleThread/ran-test/exp.r
+++ R/singleThread/ran-test/exp.r
@@ -7,8 +7,7 @@ f.long<-function(n) {
          xx<-rnorm(n)            
          log(abs(xx))+xx^2
 }
-system.time(sapply(rep(5E6,11),f.long))
-system.time(parSapply(cl,rep(5E6,11),f.long))
+
 system.time(sapply(rep(5E6,11),f.long))
 system.time(parSapply(cl,rep(5E6,11),f.long))
 
--- R/singleThread/ran-test/submit_R3.sh
+++ R/singleThread/ran-test/submit_R3.sh
@@ -7,15 +7,12 @@ date
 # here's the SGE directives
 # ------------------------------------------
 #$ -q batch.q   # <- the name of the Q you want to submit to
-#$ -pe openmpi 8  #  <- load the openmpi parallel env w/ 6 slots
+#$ -pe mpich 8  #  <- load the openmpi parallel env w/ 6 slots
 #$ -S /bin/bash   # <- run the job under bash
 #$ -N exp-test # <- name of the job in the qstat output
 #$ -o exp.out # <- name of the output file.
 #$ -j y
 #$ -cwd
-
-tmphosts=`mktemp`
-awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > $tmphosts
 
 echo "loaded modules"
 module load sge/2011.11
@@ -23,17 +20,11 @@ module load openmpi/gcc/64/1.4.5
 module load r/3.1.3
 module list
 
-echo "Got $NSLOTS slots"
-echo "jobid $JOB_ID"
-
 echo "LD_LIBRARY_PATH"
 echo $LD_LIBRARY_PATH
 
 echo "your hostfile:"
 cat $PE_HOSTFILE
-
-echo "your tmphost:"
-echo $tmphosts
 
 echo "calling mpirun now"
 mpirun -np 1 R --no-save -q < exp.r > exp.Rout

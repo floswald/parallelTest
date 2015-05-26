b5fbb8e5352f04712cd5211950f37758c93e1525
Florian Oswald <florian.oswald@gmail.com>
2015-05-26 20:42:11 +0100

    ucl econ with increasing memory load.

--- julia/ucl-econ/sge.jl
+++ julia/ucl-econ/sge.jl
@@ -1,16 +1,16 @@
 
+println("Started julia on ucl econ")
 
-println("Started julia")
+include("launcher.jl")
 
-
-# ucl_econ_launcher()
+bind_iridis_procs(8)  # bind 8 procs per node
 
 # here a function that runs your estimation:
 # using MOpt, mig
-# 
 
-println("numworkers: $(length(workers()))")
-println("workers: $(workers())")
+# require some code on all nodes
+require("../incl.jl")
+
 println("make everybody say hello")
 
 @everywhere sayhello()
@@ -21,11 +21,15 @@ pmap( i->domath(i), [100 for j in 1:length(workers())] )
 
 println("make everybody pass a memory test")
 
-pmap( i->doBIGmath(), 1:length(workers()) )
+pmap( i->doBIGmath(10000,10000), 1:length(workers()) )
+pmap( i->doBIGmath(20000,10000), 1:length(workers()) )
+pmap( i->doBIGmath(30000,10000), 1:length(workers()) )
 
 println("trying parallel for loop with $(nprocs()) processes")
-@time map( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
-@time pmap( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:20]);
+println("numworkers: $(length(workers()))")
+println("workers: $(workers())")
+@time map( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:32]);
+@time pmap( n -> sum(svd(rand(n,n))[1]) , [800 for i in 1:32]);
 
 println(" quitting ")
 

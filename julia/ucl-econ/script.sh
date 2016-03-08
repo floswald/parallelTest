54d77cc10d94d75aff564ae8b6e9d045c4947461
florian oswald <florian.oswald@gmail.com>
2016-03-08 22:02:24 +0100

    increased numnodes

--- julia/ucl-econ/script.sh
+++ julia/ucl-econ/script.sh
@@ -9,7 +9,7 @@ module load sge/2011.11
 # here's the SGE directives
 # ------------------------------------------
 #$ -q batch.q   # <- the name of the Q you want to submit to
-#$ -pe mpich 14  #  <- load the openmpi parallel env w/ $(arg1) slots
+#$ -pe mpich 24  #  <- load the openmpi parallel env w/ $(arg1) slots
 #$ -S /bin/bash   # <- run the job under bash
 #$ -N mpi-testing # <- name of the job in the qstat output
 #$ -o test.out # direct output stream to here

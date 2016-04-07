95332c8279049a6343226bb692ffe619e449db64
florian oswald <florian.oswald@gmail.com>
2016-04-07 14:56:07 +0200

    added AWS script

---
+++ julia/AWS/submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# here's the SGE directives
+# ------------------------------------------
+#$ -pe orte 2  #  <- load the openmpi parallel env w/ $(arg1) slots
+#$ -N julia-test # <- name of the job in the qstat output
+#$ -o test.out # direct output stream to here
+#$ -e test.err # <- name of the stderr file.
+#$ -cwd
+
+echo "submit script is calling julia now:"
+
+julia --machinefile $PE_HOSTFILE sge.jl
+

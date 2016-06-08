76be6b1c7c3e4b3d6841e06076c8bfe941ca6e34
Florian Oswald <florian.oswald@gmail.com>
2016-06-08 15:07:07 +0100

    added ucl CS fix

--- julia/ucl-cs/sub.sh
+++ julia/ucl-cs/sub.sh
@@ -1,28 +1,36 @@
-#$ -S /bin/bash
-#$ -j y
-#$ -N juliaMPI_example
+#   This is the most basic QSUB file needed for this cluster.
+#   Further examples can be found under /share/apps/examples
+#   Most software is NOT in your PATH but under /share/apps
+#
+#   For further info please read http://hpc.cs.ucl.ac.uk
+#
+#   NOTE hash dollar is a scheduler diredctive not a comment.
 
-#$ -l h_vmem=2G
-#$ -l tmem=2G
-#$ -l h_rt=
 
-#$ -pe orte 16
+# These are flags you must include - Two memory and one runtime.
+# Runtime is either seconds or hours:min:sec
+
+#$ -l tmem=4G
+#$ -l h_vmem=4G
+#$ -l h_rt=300
+
+#$ -pe mpich 30
 #$ -R y
 
-date
+#These are optional flags but you problably want them in all jobs
+
+#$ -S /bin/bash
+#$ -j y
+#$ -N julia-test
+#$ -cwd
+
+#The code you want to run now goes here.
+
 hostname
+date
+awk '{ for (i=0; i < $2; ++i) { print $1} }' $PE_HOSTFILE > hosts
 
-source ~/.bash_profile
-
-export PATH=/share/apps/openmpi-1.8.1/bin:$PATH
-export LD_LIBRARY_PATH=/share/apps/openmpi-1.8.1/lib:$LD_LIBRARY_PATH
-
-
-#Run julia
-echo $PE_HOSTFILE
-
-echo ""
-echo "calling julia now"
-
-julia --machinefile $PE_HOSTFILE sge.jl
+echo "your hosts"
+cat hosts
+/home/uctpfos/thirdparty/juliabin-0.4.6/bin/julia --machinefile hosts sge.jl
 

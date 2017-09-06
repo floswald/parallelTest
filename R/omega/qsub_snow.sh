155cca00615c36bc3c2494746b597bca2691361e
ran <lionup@gmail.com>
2017-09-06 13:02:09 +0100

    add omega

--- R/omega/qsub_snow.sh
+++ R/omega/qsub_snow.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -login
 
 #PBS -q fas_devel
 #PBS -N parallelTest
@@ -15,12 +16,14 @@ module load Apps/R/3.2.2-generic
 module load Libs/GSL/2.3
 module load Rpkgs/RMPI/0.6.3
 
-##module load openmpi/gcc/2.0.2
-##export OMP_NUM_THREADS=1
-###run from directory the job is submitted from
+#module load openmpi/gcc/2.0.2
+#export OMP_NUM_THREADS=1
+#run from directory the job is submitted from
 cd $PBS_O_WORKDIR
 
+NPROCS=wc -l < $PBS_NODEFILE
 
-##mpiexec -n 1 R --no-save -q -f snow_example.R
-mpirun -np 1 ~/R/x86_64-pc-linux-gnu-library/3.2/snow/RMPISNOW -q < exp.r
+#mpiexec -n 1 R --no-save -q -f snow_example.R
+#mpirun -np 1 ~/R/x86_64-pc-linux-gnu-library/3.2/snow/RMPISNOW -q < exp.r
 #mpiexec -n 1 ~/R/x86_64-pc-linux-gnu-library/3.2/snow/RMPISNOW -q < exp.r
+mpiexec -n $NPROCS ~/R/x86_64-pc-linux-gnu-library/3.2/snow/RMPISNOW -q < exp.r

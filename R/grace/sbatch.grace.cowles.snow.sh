435374c0759cadf352446fbe22d5101485f78599
ran <lionup@gmail.com>
2021-09-28 09:33:27 +0100

    testGrace

--- R/grace/sbatch.grace.cowles.snow.sh
+++ R/grace/sbatch.grace.cowles.snow.sh
@@ -28,11 +28,6 @@
 #! Optionally modify the environment seen by the application
 #! (note that SLURM reproduces the environment at submission irrespective of ~/.bashrc):
 module purge                               # Removes all modules still loaded
-#module load Apps/R/3.3.2-generic
-#module load Libs/GSL/2.3
-#module load Rpkgs/RMPI/0.6.3
-#module load R/3.5.3-foss-2018a-X11-20180131
-#module load GSL/2.3-GCCcore-6.4.0
 module load miniconda
 source activate parallel_r
 
@@ -48,10 +43,8 @@ workdir="$SLURM_SUBMIT_DIR"  # The value of SLURM_SUBMIT_DIR sets workdir to the
 #! Number of MPI tasks to be started by the application per node and in total (do not change):
 np=$SLURM_NTASKS
 
-CMD="mpirun -np 1 R --no-save -q < $options"
-#CMD="mpirun -np 1 ~/R/x86_64-pc-linux-gnu-library/3.3/snow/RMPISNOW -q < $options"
-#CMD="mpirun -np $np ~/R/x86_64-pc-linux-gnu-library/3.3/snow/RMPISNOW -q < $options"
-#CMD="mpirun -np 1 R --slave -f $options"
+#CMD="mpirun R --no-save -q < $options"
+CMD="mpirun R --slave --no-restore -f $options"
 
 ###############################################################
 ### You should not have to change anything below this line ####
--- R/grace/sbatch.grace.cowles.snow_2020.sh
+++ R/grace/sbatch.grace.cowles.snow_2020.sh
@@ -7,13 +7,18 @@
 #! sbatch directives begin here ###############################
 #! Name of the job:
 #SBATCH -J js1
+
 #SBATCH --ntasks=20
+
 #! How much wallclock time will be required?
 #SBATCH --time=1:00:00
+
 #! What types of email messages do you wish to receive?
 #SBATCH --mail-type=FAIL
+
 #! Do not change:
 #SBATCH -p pi_cowles
+
 #! sbatch directives end here (put any additional directives above this line)
 
 #! ############################################################
@@ -23,6 +28,11 @@
 #! Optionally modify the environment seen by the application
 #! (note that SLURM reproduces the environment at submission irrespective of ~/.bashrc):
 module purge                               # Removes all modules still loaded
+#module load Apps/R/3.3.2-generic
+#module load Libs/GSL/2.3
+#module load Rpkgs/RMPI/0.6.3
+#module load R/3.5.3-foss-2018a-X11-20180131
+#module load GSL/2.3-GCCcore-6.4.0
 module load miniconda
 source activate parallel_r
 
@@ -35,7 +45,13 @@ options="snow.r"
 workdir="$SLURM_SUBMIT_DIR"  # The value of SLURM_SUBMIT_DIR sets workdir to the directory
                              # in which sbatch is run.
 
-CMD="mpirun R --no-save -q < $options"
+#! Number of MPI tasks to be started by the application per node and in total (do not change):
+np=$SLURM_NTASKS
+
+CMD="mpirun -np 1 R --no-save -q < $options"
+#CMD="mpirun -np 1 ~/R/x86_64-pc-linux-gnu-library/3.3/snow/RMPISNOW -q < $options"
+#CMD="mpirun -np $np ~/R/x86_64-pc-linux-gnu-library/3.3/snow/RMPISNOW -q < $options"
+#CMD="mpirun -np 1 R --slave -f $options"
 
 ###############################################################
 ### You should not have to change anything below this line ####
